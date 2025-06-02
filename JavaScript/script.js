643cec6532c55f3e3047b9ea656c92bb088a7ddb
IsabelaYamauchi2 <isaaby.artdesign@gmail.com>
2025-06-02 16:35:58 -0300

    Animação da header ( ao descer a pagina, o header fica menor)

---
+++ JavaScript/script.js
@@ -0,0 +1,13 @@
+/*animação do header*/
+
+const header = document.querySelector('.header');
+
+function handleScroll() {
+    if(window.scrollY > 20) {
+        header.classList.add('shrink');
+    } else {
+        header.classList.remove('shrink');
+    }
+}
+
+window.addEventListener('scroll', handleScroll);
