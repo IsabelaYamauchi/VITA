eee7b36555431f5cb852494833e73f3033cb353d
IsabelaYamauchi2 <isaaby.artdesign@gmail.com>
2025-06-03 15:47:54 -0300

    Inicio do JS do formulario

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -12,4 +12,34 @@ function handleScroll() {
 
 window.addEventListener('scroll', handleScroll);
 
-/* */
+/* Formulario */
+
+const formLogin = document.getElementById("formLogin");
+const loginEmail = document.getElementById("loginEmail");
+const loginSenha = documento.getElementById("loginSenha");
+
+const formRegistro = document.getElementById("formRegistro");
+const emailRegistro = document.getElementById("emailRegistro");
+const emailRepete = document.getElementById("emailRepete");
+const senhaRegistro = document.getElementById("senhaRegistro");
+
+/* Login*/
+
+formLogin.addEventListener ("submit"), function (event) {
+    event.preventDefault();
+}
+
+    const emailValue = loginEmail.value.trim();
+    const senhaValue = loginSenha.value.trim();
+
+    if (!emailValue || !senhaValue){
+        showError("Preencha o e-mail e senha para continuar.");
+        return;
+    }
+
+    if(!isValidEmail(emailValue)) {
+        showError("Formato de e-mail inválido. Verifique e tente novamente.");
+        return;
+    }
+
+    
